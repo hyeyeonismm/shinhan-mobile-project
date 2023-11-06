b525d25de099bab699bc562069d69144903e4957
안정민 <anjeongmin@anjeongmin-ui-MacBookAir.local>
2023-11-06 22:00:11 +0900

    add : nasdaq 추가

--- App.js
+++ App.js
@@ -17,13 +17,60 @@ import {
 } from "@expo/vector-icons";
 import { LinearGradient } from "expo-linear-gradient";
 import Container from "./Container";
+import KospiContainer from "./KospiContainer";
+import NasdaqContainer from "./NasdaqContainer";
 
 export default function App() {
   const [selectedTab, setSelectedTab] = useState("국내주식");
 
+  const kospi = [
+    {
+      text: "코스피",
+      value: "2,368.34",
+      rate: "▲25.22 1.08%",
+      who1: "-3,043",
+      who2: "1,348",
+      who3: "1,634",
+    },
+    {
+      text: "코스닥",
+      value: "782.05",
+      rate: "▲9.21 1.19%",
+      who1: "1,454",
+      who2: "-1,184",
+      who3: "-454",
+    },
+  ];
+
+  const nasdaq = [
+    {
+      text: "다우 산업",
+      value: "34,061.32",
+      rate1: "▲222.24",
+      rate2: "0.66%",
+    },
+    {
+      text: "나스닥 종합",
+      value: "13,588.83",
+      rate1: "▲183.69",
+      rate2: "1.37%",
+    },
+    {
+      text: "상해 종합",
+      value: "3,474.90",
+      rate1: "▲7.77",
+      rate2: "0.22%",
+    },
+    {
+      text: "니케이 225",
+      value: "29,357.82",
+      rate1: "▲179.08",
+      rate2: "0.61%",
+    },
+  ];
+
   const handleTabClick = (tab) => {
     setSelectedTab(tab);
-    console.log(tab);
   };
 
   return (
@@ -72,38 +119,19 @@ export default function App() {
             </LinearGradient>
           </TouchableOpacity>
         </View>
-        <View style={styles.kospiWrap}>
-          <View style={styles.kospiContainer}>
-            <Text style={styles.kospiText}>코스피</Text>
-            <Text style={[styles.kospiValue, styles.red]}>2,368.34</Text>
-            <Text style={[styles.kospiRate, styles.red]}>▲25.22 1.08%</Text>
-            <View style={styles.kospiWhoWrap}>
-              <Text style={styles.kospiWho}>개인</Text>
-              <Text style={styles.kospiWho}>외국인</Text>
-              <Text style={styles.kospiWho}>기관</Text>
-            </View>
-            <View style={styles.kospiWhoWrap}>
-              <Text style={[styles.kospiWho, styles.blue]}>-3,043</Text>
-              <Text style={[styles.kospiWho, styles.red]}>1,348</Text>
-              <Text style={[styles.kospiWho, styles.red]}>1,634</Text>
-            </View>
+        {selectedTab === "국내주식" ? (
+          <View style={styles.kospiWrap}>
+            {kospi.map((item, index) => (
+              <KospiContainer key={index} {...item} />
+            ))}
           </View>
-          <View style={styles.kospiContainer}>
-            <Text style={styles.kospiText}>코스닥</Text>
-            <Text style={[styles.kospiValue, styles.red]}>782.05</Text>
-            <Text style={[styles.kospiRate, styles.red]}>▲9.21 1.19%</Text>
-            <View style={styles.kospiWhoWrap}>
-              <Text style={styles.kospiWho}>개인</Text>
-              <Text style={styles.kospiWho}>외국인</Text>
-              <Text style={styles.kospiWho}>기관</Text>
-            </View>
-            <View style={styles.kospiWhoWrap}>
-              <Text style={[styles.kospiWho, styles.blue]}>1,454</Text>
-              <Text style={[styles.kospiWho, styles.red]}>-1,184</Text>
-              <Text style={[styles.kospiWho, styles.red]}>-454</Text>
-            </View>
+        ) : (
+          <View style={styles.nasdaqWrap}>
+            {nasdaq.map((item, index) => (
+              <NasdaqContainer key={index} {...item} />
+            ))}
           </View>
-        </View>
+        )}
 
         <View style={styles.stockList}>
           <View style={styles.stockQuickNav}>
@@ -181,6 +209,7 @@ export default function App() {
             </View>
           </View>
         </View>
+
         <Container />
       </ScrollView>
     </View>
@@ -202,6 +231,7 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between",
     paddingTop: 33,
+    paddingBottom: 15,
   },
   body: {
     flexGrow: 1,
@@ -215,7 +245,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     height: 40,
     backgroundColor: "#EEE",
-    width: "90%",
+    width: "100%",
     borderRadius: 7,
     margin: 20,
     padding: 5,
@@ -239,11 +269,9 @@ const styles = StyleSheet.create({
     gap: 15,
     marginBottom: 20,
   },
-  kospiContainer: {
-    flex: 1,
-    borderRadius: 20,
-    backgroundColor: "#fff",
-    padding: 15,
+  nasdaqWrap: {
+    flex: 1.9,
+    marginBottom: 20,
     shadowColor: "#343434",
     shadowOffset: {
       width: 0,
@@ -251,28 +279,7 @@ const styles = StyleSheet.create({
     },
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
-
     elevation: 5,
-  },
-  kospiText: {
-    fontSize: 15,
-    color: "#000",
-    marginBottom: 10,
-  },
-  kospiValue: {
-    fontSize: 20,
-  },
-  kospiRate: {
-    fontSize: 12,
-    marginBottom: 15,
-  },
-  kospiWhoWrap: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-  },
-  kospiWho: {
-    fontSize: 12,
-    color: "#808080",
   },
   red: {
     color: "#CB0B37",
@@ -289,9 +296,8 @@ const styles = StyleSheet.create({
   stockQuickNav: {
     width: "100%",
     flexDirection: "row",
-    marginBottom: 10,
+    marginBottom: 20,
     gap: 10,
-    padding: 15,
   },
   stockQuickNavTab: {
     width: "30%",
@@ -301,6 +307,7 @@ const styles = StyleSheet.create({
     shadowColor: "#343434",
     alignItems: "center",
     justifyContent: "center",
+    paddingVertical: 8,
     shadowOffset: {
       width: 0,
       height: 2,
@@ -311,18 +318,17 @@ const styles = StyleSheet.create({
     elevation: 5,
   },
   recentStockHeader: {
-    paddingHorizontal: 15,
     marginBottom: 10,
   },
   recentStockHeaderText: {
-    fontSize: 16,
-    color: "#343434",
+    color: "#3A697B",
+    fontSize: 18,
+    fontWeight: "700",
   },
   recentStockContainer: {
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-between",
-    paddingHorizontal: 15,
     paddingVertical: 8,
     borderRadius: 10,
     backgroundColor: "#fff",
